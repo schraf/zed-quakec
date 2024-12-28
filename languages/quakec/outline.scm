13298f5e1e7491922d1a441cf0061671ee62a242
Marc Schraffenberger <marc@schraffenberger.com>
2024-12-28 13:46:41 -0500

    initial ci

---
+++ languages/quakec/outline.scm
@@ -0,0 +1,16 @@
+
+(
+    (function_declaration) @name
+) @item
+
+(
+    (function_definition) @name
+) @item
+
+(
+    (field_definition) @name
+) @item
+
+(
+    (variable_definition) @name
+) @item
